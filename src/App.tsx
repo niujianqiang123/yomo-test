2f21cea27ee136f18e77b7fb29e49c9add28ffef
牛建强 <niujianqiang@haima.me>
2022-07-17 11:44:55 +0800

    ts init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CursorComp from './Cursor';
+
+function App() {
+  return (
+    <div className="App">
+      <CursorComp />
+    </div>
+  );
+}
+
+export default App;
